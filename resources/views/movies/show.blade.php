5a8a76b151bad90c43b8fb94d41554096512e194
danila dolmatov <danila@autosport>
2019-08-12 11:49:20 -0400

    display trailer and fix issue if empty descriptions

--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -4,14 +4,11 @@
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mt-3 mb-2">
-        <h1 class="h3 mb-0 text-gray-800">{{ $movie->title_ru }} <br/><small>{{ $movie->title_en  }}</small></h1>
+        <h1 class="h3 mb-0 text-gray-800">Смотреть {{ $movie->title_ru }} <br/><small>{{ $movie->title_en  }}</small></h1>
         {{--                    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>--}}
     </div>
 
-
     <div class="row">
-
-
 
         <!-- Movie.player -->
         <div class="col-lg-9">
@@ -26,13 +23,26 @@
                 </div>
 
             </div>
-            <h3>{{ $movie->tagline }}</h3>
-            <p>
-                {{ $movie->description }}
-            </p>
+            <h4>{{ $movie->tagline }}</h4>
+            <h5>{{ $movie->description }}</h5>
+
+        <!-- Trailer.player -->
+        @if ($movie->trailer_iframe_url)
+            <hr />
+            <h4>Трейлер к {{ $movie->title_ru }}</h4>
+            <div class="card shadow mb-4" >
+                <div class="embed-responsive embed-responsive-21by9">
+                    {{--                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/F40TxFHNPQA" allowfullscreen=""></iframe>--}}
+                    {{--<iframe class="embed-responsive-item" src="https://player.vimeo.com/video/137857207" allowfullscreen></iframe>--}}
+                    {{--<video class="video-fluid z-depth-1" autoplay loop controls muted>
+                        <source src="https://mdbootstrap.com/img/video/Sail-Away.mp4" type="video/mp4" />
+                    </video>--}}
+                    <iframe class="embed-responsive-item" src="{{ $movie->trailer_iframe_url }}" allowfullscreen=""></iframe>
+                </div>
+
+            </div>
         </div>
-
-
+        @endif
 
         <!-- Poster -->
         <div class="col-lg-3">
@@ -41,14 +51,14 @@
                      style="max-height: 375px">
             </div>
 
-            <p>Жанры: {{ $genres }}</p>
-            <p>Год: {{ $movie->year }}</p>
-            <p>Рейтинг: {{ $movie->kinopoisk_rating }}</p>
-            <p>Голосов: {{ $movie->kinopoisk_votes }}</p>
-            <p>Актеры: {{ $actors }}</p>
-            <p>Директор: {{ $directors }}</p>
-            <p>Страна: {{ $countries }}</p>
-            <p>Возраст: {{ $movie->age }}</p>
+            @if($genres)<p>Жанры: {{ $genres }}</p>@endif
+            @if($movie->year)<p>Год: {{ $movie->year }}</p>@endif
+            @if($movie->kinopoisk_rating)<p>Рейтинг: {{ $movie->kinopoisk_rating }}</p>@endif
+            @if($movie->kinopoisk_votes)<p>Голосов: {{ $movie->kinopoisk_votes }}</p>@endif
+            @if($actors)<p>Актеры: {{ $actors }}</p>@endif
+            @if($directors)<p>Директор: {{ $directors }}</p>@endif
+            @if($countries)<p>Страна: {{ $countries }}</p>@endif
+            @if($movie->age)<p>Возраст: {{ $movie->age }}</p>@endif
         </div>
 
 
